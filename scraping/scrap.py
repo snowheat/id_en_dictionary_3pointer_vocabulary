72a948272c1b90d213aa36400c9b7c89384bf351
snowheat <musiklib@gmail.com>
2018-02-08 08:32:59 +0700

    pickle created

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -1,4 +1,4 @@
-import requests, pymysql.cursors, string
+import requests, pymysql.cursors, string, re, pickle
 from bs4 import BeautifulSoup
 from nltk import word_tokenize
 
@@ -6,49 +6,10 @@ class Scraper:
 
 	def __init__(self):
 		indonesian_words_unique_token = []
-		indonesian_words_token = word_tokenize(self.__get_indonesian_words())
-		for word in indonesian_words_token:
-			if word not in indonesian_words_unique_token:
-				indonesian_words_unique_token.append(word)
-		
-		print(len(indonesian_words_token))
-		print(len(indonesian_words_unique_token))
+		PIK = "indonesian_words_unique_token.pickle"
+		with open(PIK, "rb") as f:
+			indonesian_words_unique_token = pickle.load(f)
+
 		print(indonesian_words_unique_token)
-
-	def __get_indonesian_words(self):
-		conn_kamus = pymysql.connect(
-			host='localhost',
-			user='root',
-			password='',
-			db='kamus',
-			charset='utf8mb4',
-			cursorclass=pymysql.cursors.DictCursor)
-
-
-		indonesian_words = ""	
-
-		try:
-			with conn_kamus.cursor() as cursor:
-				# Read a single record
-				sql = """
-					SELECT s.song_lyric
-					FROM musiklib.song s 
-					INNER JOIN musiklib.artist t
-					ON s.song_artist_id = t.artist_id
-					WHERE t.artist_country_id = 1
-					AND s.song_lyric NOT LIKE %s
-					LIMIT 1000
-				"""
-				cursor.execute(sql, ('%you%',))
-				result = cursor.fetchall()
-				for lyric in result:
-					indonesian_words += lyric['song_lyric']
-					indonesian_words += " "
-				
-		finally:
-			conn_kamus.close()
-
-		indonesian_words = indonesian_words.replace('<br>', ' ').lower()
-		indonesian_words = indonesian_words.replace('.', ' ').lower()
 		
-		return indonesian_words
+	
